11b2330d7ee6d28b0faf0602b04fa4266dec288e
Johnzhang <zhangzhenxiang@126.com>
2022-03-29 17:38:59 +0800

    Fix Intervention image aspeclRatio issuze

--- backend/app/Service/ImageResizeService.php
+++ backend/app/Service/ImageResizeService.php
@@ -19,15 +19,25 @@ class ImageResizeService{
 	}
 
 	//Resize a image with a specific width and aspecl ratio
-	public function resize($src_image_path = '', $dst_image_path = '', $resize_to_width = 0){
+	public function resize($src_image_path = '', $dst_image_path = '', $dst_image_width = 0){
 		$image = InterventionImage::make($src_image_path);
 		$image_fname = basename($src_image_path);
 
-		$image->resize($resize_to_width, null, function($constraint){
+		//Don't know why the aspeclRatio does not support. It may be library version issue
+		/*$image->resize($resize_to_width, null, function($constraint){
 			$constraint->aspeclRatio();
 			$constraint->upsize();
-		});
+		});*/
 
+		$src_image_width = $image->width();
+		$src_image_height = $image->height();
+		$img_wh_ratio = $src_image_width / $src_image_height;
+
+		//calc the dst image height based on the ratio and width
+		$dst_image_height = $dst_image_width / $img_wh_ratio;
+
+		//resize it and save it
+		$image->resize($dst_image_width, $dst_image_height);
 		$image->save($dst_image_path.$image_fname);
 	}
 }
